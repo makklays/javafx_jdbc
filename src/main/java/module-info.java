cc9c6510279c1c21f1203118b218bd9de25f8388
Alexander <makklays@gmail.com>
2023-12-11 23:33:07 +0100

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.example.demo3 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires java.sql;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.demo3 to javafx.fxml;
+    exports com.example.demo3;
+}
